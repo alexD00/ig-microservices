f934b3c4cab7d2ce76a8d8ceceedd331cb40078d
Alex Rodriguez <alex0807miami@gmail.com>
2024-12-28 19:54:02 -0500

    add UserService interface

--- services/user/src/main/java/com/alex/user/service/UserService.java
+++ services/user/src/main/java/com/alex/user/service/UserService.java
@@ -2,60 +2,19 @@ package com.alex.user.service;
 
 import com.alex.user.dto.UserRequest;
 import com.alex.user.dto.UserResponse;
-import com.alex.user.mapper.UserMapper;
-import com.alex.user.model.User;
-import com.alex.user.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    UserResponse createUser(@Valid UserRequest userRequest);
 
-    public UserResponse createUser(@Valid UserRequest userRequest) {
-        User user = userMapper.toUser(userRequest);
+    UserResponse updateUser(Integer userId, @Valid UserRequest userRequest);
 
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
+    UserResponse findUserById(Integer userId);
 
-    public UserResponse updateUser(Integer userId, @Valid UserRequest userRequest){
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("User with id: " + userId + " was not found"));
+    List<UserResponse> findAllUsers();
 
-        user.setFirstName(userRequest.firstName());
-        user.setLastName(userRequest.lastName());
-        user.setEmail(userRequest.email());
-        user.setPassword(userRequest.password());
-
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
-
-    public UserResponse findUserById(Integer userId){
-        return userRepository.findById(userId)
-                .map(userMapper::toUserResponse)
-                .orElseThrow(() -> new EntityNotFoundException("User with id: " + userId + " was not found"));
-    }
-
-    public List<UserResponse> findAllUsers(){
-        return userRepository.findAll()
-                .stream()
-                .map(userMapper::toUserResponse)
-                .toList();
-    }
-
-    public String deleteUserById(Integer userId){
-        if (!userRepository.existsById(userId)){
-            throw new EntityNotFoundException("User with id: " + userId + " was not found");
-        }
-        userRepository.deleteById(userId);
-
-        return "Deleted user with id: " + userId;
-    }
+    String deleteUserById(Integer userId);
 }
---
+++ services/user/src/main/java/com/alex/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.alex.user.service.impl;
+
+import com.alex.user.dto.UserRequest;
+import com.alex.user.dto.UserResponse;
+import com.alex.user.mapper.UserMapper;
+import com.alex.user.model.User;
+import com.alex.user.repository.UserRepository;
+import com.alex.user.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserResponse createUser(@Valid UserRequest userRequest) {
+        User user = userMapper.toUser(userRequest);
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public UserResponse updateUser(Integer userId, @Valid UserRequest userRequest){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User with id: " + userId + " was not found"));
+
+        user.setFirstName(userRequest.firstName());
+        user.setLastName(userRequest.lastName());
+        user.setEmail(userRequest.email());
+        user.setPassword(userRequest.password());
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public UserResponse findUserById(Integer userId){
+        return userRepository.findById(userId)
+                .map(userMapper::toUserResponse)
+                .orElseThrow(() -> new EntityNotFoundException("User with id: " + userId + " was not found"));
+    }
+
+    public List<UserResponse> findAllUsers(){
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toUserResponse)
+                .toList();
+    }
+
+    public String deleteUserById(Integer userId){
+        if (!userRepository.existsById(userId)){
+            throw new EntityNotFoundException("User with id: " + userId + " was not found");
+        }
+        userRepository.deleteById(userId);
+
+        return "Deleted user with id: " + userId;
+    }
+}
