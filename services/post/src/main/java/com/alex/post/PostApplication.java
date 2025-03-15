bd5d01cd6e89bd845eba0e68f1728ef413cc76a3
Alex Rodriguez <alex0807miami@gmail.com>
2025-03-14 22:36:23 -0500

    add starting log to Post service

--- services/post/src/main/java/com/alex/post/PostApplication.java
+++ services/post/src/main/java/com/alex/post/PostApplication.java
@@ -1,5 +1,6 @@
 package com.alex.post;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,10 +9,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
+@Slf4j
 public class PostApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostApplication.class, args);
+		log.info("Post service started successfully...");
 	}
 
 }
